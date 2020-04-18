ee309e01a5c9047908981d1274c8096b3d3e9fda
Kamen Kitanov <kitanovkamen@gmail.com>
2020-04-18 14:47:07 +0300

    added basic player movement

---
+++ Assets/Player/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float Speed = 1.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontalAxis = Input.GetAxis("Horizontal");
+        float verticalAxis = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.right * horizontalAxis * Speed * Time.deltaTime);
+        transform.Translate(Vector3.up * verticalAxis * Speed * Time.deltaTime);
+    }
+}
