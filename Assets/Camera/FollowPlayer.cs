2e66e9bf92fc8d0765e9a822839d24d39c8f947e
Kamen Kitanov <kitanovkamen@gmail.com>
2020-04-18 16:14:36 +0300

    Added Camera following the player Added walls

---
+++ Assets/Camera/FollowPlayer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public GameObject PlayerObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(PlayerObject.transform.position.x, PlayerObject.transform.position.y, transform.position.z);
+    }
+}
